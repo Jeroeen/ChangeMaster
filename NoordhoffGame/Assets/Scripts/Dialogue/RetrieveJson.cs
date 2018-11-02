d960f831e4e8c7930cd7649172420e320324fafb
Jeroen v Dijk <jeroftw@gmail.com>
2018-11-02 13:20:36 +0100

    Level 1 omgeving + dialoog nagenoeg af. Mist alleen nog tweede dialoog

--- NoordhoffGame/Assets/Scripts/Dialogue/RetrieveJson.cs
+++ NoordhoffGame/Assets/Scripts/Dialogue/RetrieveJson.cs
@@ -18,7 +18,9 @@ public class RetrieveJson
         {
             path = "DialogueFiles/" + nameOfPartner + "-" + stage + "-" + dialogueCount;
         }
-        
+
+		Debug.Log(path);
+
         TextAsset asset = Resources.Load(path) as TextAsset;
         string jsonString = asset.ToString();
 
