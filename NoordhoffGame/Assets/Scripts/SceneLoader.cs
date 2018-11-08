5e77d4993139cdf86038c66d54049435c979d67d
Jeroen v Dijk <jeroftw@gmail.com>
2018-11-08 11:57:35 +0100

    terug naar de brug toegevoegd

---
+++ NoordhoffGame/Assets/Scripts/SceneLoader.cs
@@ -0,0 +1,63 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+namespace Assets.Scripts
+{
+	public class SceneLoader : MonoBehaviour
+	{
+		private bool isFading;
+
+		[SerializeField] private Transition transition;
+		[SerializeField] private string SceneToLoad;
+
+		[SerializeField] private Button settingsButton;
+		[SerializeField] private Button infoButton;
+		[SerializeField] private Button interventionButton;
+
+		void Start()
+		{
+			ChangeInteractability(false);
+		}
+
+		void Update()
+		{
+			if (!isFading) return;
+
+			if (!transition.transform.gameObject.activeSelf)
+			{
+				transition.transform.gameObject.SetActive(true);
+			}
+
+			if (!transition.FadeOut()) return;
+
+			if (!string.IsNullOrWhiteSpace(SceneToLoad))
+			{
+				// Load specific scene
+				SceneManager.LoadScene(SceneToLoad);
+			}
+			// Load bridge
+			SceneManager.LoadScene(3);
+		}
+
+		public void StartFading()
+		{
+			isFading = true;
+		}
+
+		private void ChangeInteractability(bool boolean)
+		{
+			settingsButton.interactable = boolean;
+			infoButton.interactable = boolean;
+			interventionButton.interactable = boolean;
+		}
+
+		public void Close()
+		{
+			ChangeInteractability(true);
+
+			gameObject.SetActive(false);
+		}
+			
+	}
+}
