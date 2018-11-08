60e3a0cf4b2a0151d07f6ad155a9cc487f968dd3
Jeroen v Dijk <jeroftw@gmail.com>
2018-11-08 15:29:59 +0100

    added world map

---
+++ NoordhoffGame/Assets/Scripts/DetermineDestination.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+namespace Assets.Scripts
+{
+	public class DetermineDestination : MonoBehaviour
+	{
+		public void DestinationClick(GameObject destination)
+		{
+			Debug.Log("x");
+			Debug.Log(destination.name);
+		}
+	}
+}
