8da1c746ded4d08d3e74440cb9266cb3fca0157f
Jeroen v Dijk <jeroftw@gmail.com>
2018-11-14 14:14:26 +0100

    determined destination for clicking on the map and updated the baseview

--- NoordhoffGame/Assets/Scripts/DetermineDestination.cs
+++ NoordhoffGame/Assets/Scripts/DetermineDestination.cs
@@ -1,15 +1,48 @@
 ﻿using UnityEngine;
+using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 
 namespace Assets.Scripts
 {
 	public class DetermineDestination : MonoBehaviour
 	{
-		[SerializeField] private Text text;
+		private bool isFading;
 
-		public void DestinationClick()
+		[SerializeField] private Text textObject;
+		[SerializeField] private GameObject confirmDialogue;
+		[SerializeField] private GameObject uiElements;
+		[SerializeField] private Transition transition;
+
+		public void DestinationClick(GameObject obj)
 		{
-			Debug.Log(gameObject.name);
+			textObject.text = obj.name;
+			confirmDialogue.SetActive(true);
+		}
+
+		public void Confirm()
+		{
+			isFading = true;
+		}
+
+		void Update()
+		{
+			if (!isFading)
+			{
+				return;
+			}
+
+			if (!transition.transform.gameObject.activeSelf)
+			{
+				uiElements.SetActive(false);
+				transition.transform.gameObject.SetActive(true);
+			}
+			else if (transition.FadeOut())
+			{
+				// This will load the next scene once saving and loading the game is implemented
+				// Must also save the next destination so it can be used in the future.
+				SceneManager.LoadScene(4);
+			}
+
 		}
 	}
 }
