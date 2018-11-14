8bdcb2ebaa9c68672bf36cd56304531e75b53b6f
Gwendea <gwenvantil@gmail.com>
2018-11-14 15:15:56 +0100

    more fixes

--- NoordhoffGame/Assets/Scripts/GameSaveLoad/Game.cs
+++ NoordhoffGame/Assets/Scripts/GameSaveLoad/Game.cs
@@ -7,39 +7,39 @@ using UnityEngine.SceneManagement;
 [System.Serializable]
 public class Game
 {
-    public static Game currentGame {  set; private get; }
+    public static Game CurrentGame {  set; private get; }
     public Player Player { get; set; }
     public InfoList Information { get; set; }
-    public int lastLevelfinished { get; set; }
+    public int LastLevelFinished { get; set; }
     public Dictionary<string, bool> DialogueRead { get; set; }
 
     private Game()
     {
         Player = Player.GetPlayer();
-        lastLevelfinished = -1;
+        LastLevelFinished = -1;
         DialogueRead = new Dictionary<string, bool>();
     }
 
     public void AddLevel()
     {
-        lastLevelfinished++;
+        LastLevelFinished++;
         Information = null;
         DialogueRead = new Dictionary<string, bool>();
     }
 
     public static void SetGame(Game game)
     {
-        currentGame = game;
+        CurrentGame = game;
     }
 
     public static Game GetGame()
     {
         // if currentGame is null, make a new Game and return it. Otherwise return the existing Game
-        return currentGame ?? (currentGame = new Game());
+        return CurrentGame ?? (CurrentGame = new Game());
     }
 
     public static void ClearGame()
     {
-        currentGame = new Game();
+        CurrentGame = new Game();
     }
 }
