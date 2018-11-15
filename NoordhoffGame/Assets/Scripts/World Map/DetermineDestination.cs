06502ea2de3cca176150854ea59805e992578636
Jeroen v Dijk <jeroftw@gmail.com>
2018-11-15 09:44:00 +0100

    fixed final changes

--- NoordhoffGame/Assets/Scripts/World Map/DetermineDestination.cs
+++ NoordhoffGame/Assets/Scripts/World Map/DetermineDestination.cs
@@ -40,14 +40,12 @@ namespace Assets.Scripts
 			}
 			else if (transition.FadeOut())
 			{
-				// This will load the next scene once saving and loading the game is implemented
-				// Must also save the next destination so it can be used in the future.
 				var game = GameSaveLoad.Game.GetGame();
 				game.CurrentDestination = destinationObject.name;
 
+				// Load next level
 				SceneManager.LoadScene(game.LastFinishedLevel + 1);
 			}
-
 		}
 	}
 }
