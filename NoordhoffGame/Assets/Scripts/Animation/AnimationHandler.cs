085d3585d4430a02147dd6e8a95ce086e8a4126d
Joran <joranknol@gmail.com>
2018-11-28 12:55:28 +0100

    Bug fixing yahoo

--- NoordhoffGame/Assets/Scripts/Animation/AnimationHandler.cs
+++ NoordhoffGame/Assets/Scripts/Animation/AnimationHandler.cs
@@ -1,10 +1,12 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.EventSystems;
 
 public class AnimationHandler : MonoBehaviour
 {
     [SerializeField] private Animator animator;
+    
 
     public void ToggleAnimation()
     {
