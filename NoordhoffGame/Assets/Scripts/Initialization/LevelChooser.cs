c1e7884d04f70999cdb5a6e3b2913478a09bd000
Jeroen v Dijk <jeroftw@gmail.com>
2018-11-29 16:31:38 +0100

    no message

--- NoordhoffGame/Assets/Scripts/Initialization/LevelChooser.cs
+++ NoordhoffGame/Assets/Scripts/Initialization/LevelChooser.cs
@@ -5,8 +5,7 @@ namespace Assets.Scripts.Initialization
 {
 	public class LevelChooser : MonoBehaviour
 	{
-		// Start is called before the first frame update
-		void Start()
+		public void LoadScene()
 		{
 			string levelName = PlayerPrefs.GetString("LastLevel"); //this assumes you save a string in PlayerPrefs at some point that's the name of the scene with that level
 			if (levelName == null || levelName == "")
