5e164f0d96d4ec2aadb65bcd6f2d8b08dadee78a
Jeroen v Dijk <jeroftw@gmail.com>
2018-12-03 10:16:13 +0100

    added splashscreen and transition functionality

--- NoordhoffGame/Assets/Scripts/Initialization/LevelChooser.cs
+++ NoordhoffGame/Assets/Scripts/Initialization/LevelChooser.cs
@@ -1,18 +1,43 @@
-﻿using UnityEngine;
+﻿using Assets.Scripts.Cutscene;
+using UnityEngine;
 using UnityEngine.SceneManagement;
 
 namespace Assets.Scripts.Initialization
 {
 	public class LevelChooser : MonoBehaviour
 	{
+		[SerializeField] private Transition transition;
+
+		private bool isFading;
+		private string levelName;
+
 		public void LoadScene()
 		{
-			string levelName = PlayerPrefs.GetString("LastLevel"); //this assumes you save a string in PlayerPrefs at some point that's the name of the scene with that level
-			if (levelName == null || levelName == "")
+			if (!transition.transform.gameObject.activeSelf)
+			{
+				transition.gameObject.SetActive(true);
+			}
+
+			levelName = PlayerPrefs.GetString("LastLevel"); //this assumes you save a string in PlayerPrefs at some point that's the name of the scene with that level
+			if (string.IsNullOrEmpty(levelName))
 			{
 				levelName = "Opening Cutscene"; //the default scene that should be loaded when you play for the first time
 			}
-			SceneManager.LoadScene(levelName);
+
+			isFading = true;
+		}
+
+		void Update()
+		{
+			if (!isFading)
+			{
+				return;
+			}
+
+			if (transition.FadeOut())
+			{
+				SceneManager.LoadScene(levelName);
+			}
 		}
 	}
 }
